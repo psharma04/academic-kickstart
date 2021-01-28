1412da709d86edf288bd7e60b5505b9759fd8657
Pranav Sharma <psharma@schoolnotes.xyz>
2021-01-28 16:29:44 +1100

    idk

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,27 @@
 module.exports = {
-  plugins: [
-    require("autoprefixer")({
-      overrideBrowserslist: ["> 0.5% in US", "Safari > 9"]
-    })
-  ]
+  plugins: {
+      '@fullhuman/postcss-purgecss': {
+          content: [
+              './themes/academic/layouts/**/*.html', 
+              './themes/academic/assets/js/*.js',
+              './themes/academic/static/js/*.js',
+              './layouts/**/*.html',
+              './static/js/*.js'
+            ],
+          whitelist: [
+              'highlight',
+              'language-bash',
+              'pre',
+              'video',
+              'code',
+              'content',
+              'h3',
+              'h4',
+              'ul',
+              'li'
+          ]
+      },
+      autoprefixer: {},
+      cssnano: {preset: 'default'}
+  }
 };
